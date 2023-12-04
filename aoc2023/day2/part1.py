92ed8653127590466f2cda99e768b789119613f2
Michael T <bravmech@gmail.com>
2023-12-04 13:03:36 +0400

    Solve day 2 part 1

---
+++ aoc2023/day2/part1.py
@@ -0,0 +1,33 @@
+LIMITS = {
+    'red': 12,
+    'green': 13,
+    'blue': 14,
+}
+
+
+def parse_color(s: str) -> tuple[str, int]:
+    v, c = s.split()
+    return c, int(v)
+
+
+def parse_round(s: str) -> dict[str, int]:
+    return dict(parse_color(c) for c in s.split(', '))
+
+
+def parse_game(line: str) -> tuple[int, list[dict[str, int]]]:
+    h, rest = line.split(': ')
+    i = int(h.split()[1])
+    return i, [parse_round(r) for r in rest.split('; ')]
+
+
+def solve(text: str) -> int:
+    res = 0
+    for game in text.splitlines():
+        index, rounds = parse_game(game)
+        if all(all(r.get(c, 0) <= LIMITS[c] for c in LIMITS) for r in rounds):
+            res += index
+    return res
+
+
+if __name__ == '__main__':
+    pass
