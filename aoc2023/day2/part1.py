45f2ae1eb2712e39f25db23ebeec33294cff8356
Michael T <bravmech@gmail.com>
2023-12-05 12:43:21 +0400

    Refactor day 2

--- aoc2023/day2/part1.py
+++ aoc2023/day2/part1.py
@@ -1,8 +1,27 @@
-LIMITS = {
-    'red': 12,
-    'green': 13,
-    'blue': 14,
-}
+import dataclasses
+import typing
+
+MAX_RED = 12
+MAX_GREEN = 13
+MAX_BLUE = 14
+
+
+@dataclasses.dataclass
+class Round:
+    red: int = 0
+    green: int = 0
+    blue: int = 0
+
+    def __le__(self, other: typing.Self) -> bool:
+        return (
+            self.red <= other.red
+            and self.green <= other.green
+            and self.blue <= other.blue
+        )
+
+    @classmethod
+    def parse(cls, s: str) -> typing.Self:
+        return cls(**dict(parse_color(c) for c in s.split(', ')))
 
 
 def parse_color(s: str) -> tuple[str, int]:
@@ -10,26 +29,17 @@ def parse_color(s: str) -> tuple[str, int]:
     return c, int(v)
 
 
-def parse_round(s: str) -> dict[str, int]:
-    return dict(parse_color(c) for c in s.split(', '))
-
-
-def parse_game(line: str) -> tuple[int, list[dict[str, int]]]:
-    h, rest = line.split(': ')
-    i = int(h.split()[1])
-    return i, [parse_round(r) for r in rest.split('; ')]
-
-
-def limited(d: dict[str, int]) -> bool:
-    return all(d[c] <= LIMITS[c] for c in d)
+def parse_game(line: str) -> list[Round]:
+    _, rest = line.split(': ')
+    return [Round.parse(s) for s in rest.split('; ')]
 
 
 def solve(text: str) -> int:
+    max_round = Round(MAX_RED, MAX_GREEN, MAX_BLUE)
     res = 0
-    for game in text.splitlines():
-        index, rounds = parse_game(game)
-        if all(limited(r) for r in rounds):
-            res += index
+    for i, line in enumerate(text.splitlines(), 1):
+        if all(r <= max_round for r in parse_game(line)):
+            res += i
     return res
 
 
--- aoc2023/day2/part2.py
+++ aoc2023/day2/part2.py
@@ -1,20 +1,18 @@
-import math
-
 from . import part1
 
-COLORS = ['red', 'green', 'blue']
 
-
-def power(rounds: list[dict[str, int]]) -> int:
-    cubes = {c: max(r.get(c, 0) for r in rounds) for c in COLORS}
-    return math.prod(cubes.values())
+def power(rounds: list[part1.Round]) -> int:
+    return (
+        max(r.red for r in rounds)
+        * max(r.green for r in rounds)
+        * max(r.blue for r in rounds)
+    )
 
 
 def solve(text: str) -> int:
     res = 0
     for game in text.splitlines():
-        _, rounds = part1.parse_game(game)
-        res += power(rounds)
+        res += power(part1.parse_game(game))
     return res
 
 
