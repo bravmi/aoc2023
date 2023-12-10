e61187e61cf97934499121f7e490b90f1fba8751
Michael T <bravmech@gmail.com>
2023-12-10 16:03:32 +0400

    Refactor day 6

--- aoc2023/day6/part1.py
+++ aoc2023/day6/part1.py
@@ -11,12 +11,12 @@ def parse_races(text: str) -> list[tuple[int, int]]:
     return list(zip(parse_numbers(lines[0]), parse_numbers(lines[1])))
 
 
-def win_ways(time: int, dist: int) -> int:
+def winning_ways(time: int, dist: int) -> int:
     return sum(t * (time - t) > dist for t in range(time))
 
 
 def solve(text: str) -> int:
-    return math.prod(win_ways(t, d) for t, d in parse_races(text))
+    return math.prod(winning_ways(t, d) for t, d in parse_races(text))
 
 
 if __name__ == '__main__':
--- aoc2023/day6/part2.py
+++ aoc2023/day6/part2.py
@@ -1,4 +1,4 @@
-from . import part1
+import math
 
 
 def parse_number(line: str) -> int:
@@ -11,8 +11,16 @@ def parse_races(text: str) -> tuple[int, int]:
     return parse_number(lines[0]), parse_number(lines[1])
 
 
+def winning_ways(time: int, dist: int) -> int:
+    a, b, c = 1, -time, dist
+    d = b**2 - 4 * a * c
+    t1 = -b / 2 - (d**0.5) / 2
+    t2 = -b / 2 + (d**0.5) / 2
+    return math.floor(t2) - math.ceil(t1) + 1
+
+
 def solve(text: str) -> int:
-    return part1.win_ways(*parse_races(text))
+    return winning_ways(*parse_races(text))
 
 
 if __name__ == '__main__':
