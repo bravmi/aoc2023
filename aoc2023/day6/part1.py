9bbc6ff9d9d76c35c3161138a7dce958d0663e74
Michael T <bravmech@gmail.com>
2023-12-10 13:11:53 +0400

    Solve day 6 part 1

---
+++ aoc2023/day6/part1.py
@@ -0,0 +1,23 @@
+import math
+
+
+def parse_numbers(line: str) -> list[int]:
+    return list(map(int, line.split()[1:]))
+
+
+def parse_races(text: str) -> list[tuple[int, int]]:
+    lines = text.splitlines()
+    assert len(lines) == 2
+    return list(zip(parse_numbers(lines[0]), parse_numbers(lines[1])))
+
+
+def win_ways(time: int, dist: int) -> int:
+    return sum(t * (time - t) > dist for t in range(time))
+
+
+def solve(text: str) -> int:
+    return math.prod(win_ways(t, d) for t, d in parse_races(text))
+
+
+if __name__ == '__main__':
+    pass
