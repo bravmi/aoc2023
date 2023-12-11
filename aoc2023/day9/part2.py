100c88c5f72b1ec3eb70dcc2902190cf8e57080c
Michael T <bravmech@gmail.com>
2023-12-11 09:10:14 +0400

    Solve day 9 part 2

--- aoc2023/day9/part2.py
+++ aoc2023/day9/part2.py
@@ -1,5 +1,20 @@
+from . import part1
+
+
+def prediction(hist: list[int]) -> int:
+    seqs = [hist]
+    while not all(x == 0 for x in seqs[-1]):
+        diffs = [y - x for x, y in zip(seqs[-1], seqs[-1][1:])]
+        seqs.append(diffs)
+    seqs[-1].insert(0, 0)
+    for i in range(len(seqs) - 2, -1, -1):
+        seqs[i].insert(0, seqs[i][0] - seqs[i + 1][0])
+    return seqs[0][0]
+
+
 def solve(text: str) -> int:
-    return 0
+    histories = [part1.parse_history(line) for line in text.splitlines()]
+    return sum(prediction(hist) for hist in histories)
 
 
 if __name__ == '__main__':
--- aoc2023/day9/tests.py
+++ aoc2023/day9/tests.py
@@ -17,12 +17,11 @@ def test_part1(filename: str, expected: int):
     assert part1.solve(text) == expected
 
 
-@pytest.mark.skip
 @pytest.mark.parametrize(
     'filename, expected',
     [
-        ('example.txt', ...),
-        ('input.txt', ...),
+        ('example.txt', 2),
+        ('input.txt', 993),
     ],
 )
 def test_part2(filename: str, expected: int):
