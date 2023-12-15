f3ac6fc220369bbedb75039b48a617b525f61bbd
Michael T <bravmech@gmail.com>
2023-12-15 18:13:46 +0400

    Refactor day 12 part 1

--- aoc2023/day12/part1.py
+++ aoc2023/day12/part1.py
@@ -1,56 +1,33 @@
 import dataclasses
+import typing
 
 
 @dataclasses.dataclass
 class Record:
-    start: int
-    unknowns: list[int]
+    pattern: str
     damaged: list[int]
-    _len_unknowns: int = 0
-    _len_damaged: int = 0
 
     @classmethod
-    def parse(cls, line: str) -> 'Record':
+    def parse(cls, line: str) -> typing.Self:
         left, right = line.split()
-        start = sum(1 << i for i, c in enumerate(left[::-1]) if c == '#')
-        unknowns = [i for i, c in enumerate(left[::-1]) if c == '?']
-        damaged = [int(s) for s in right.split(',')]
-        return cls(start, unknowns, damaged)
-
-    def __post_init__(self):
-        self._len_unknowns = len(self.unknowns)
-        self._len_damaged = len(self.damaged)
-
-    def match(self, state: int) -> bool:
-        count = 0
-        i = self._len_damaged - 1
-        while state:
-            if state & 1:
-                count += 1
-            else:
-                if count:
-                    if i < 0 or count != self.damaged[i]:
-                        return False
-                    count = 0
-                    i -= 1
-            state >>= 1
-        if count and count != self.damaged[i]:
-            return False
-        return i == 0
+        return cls(left, [int(x) for x in right.split(',')])
 
     def arrange(self) -> int:
-        count = 0
-        for mask in range(1 << self._len_unknowns):
-            state = self.start
-            for i, u in enumerate(self.unknowns):
-                if mask & (1 << i):
-                    state |= 1 << u
-            if self.match(state):
-                count += 1
-        return count
-
-    def __str__(self) -> str:
-        return f'{self.start:b} {self.unknowns} {self.damaged}'
+        groups = [[0]]
+        for c in self.pattern:
+            match c:
+                case '#':
+                    for g in groups:
+                        g[-1] += 1
+                case '.':
+                    for g in groups:
+                        g.append(0)
+                case '?':
+                    for g in groups.copy():
+                        groups.append(g + [0])
+                        g[-1] += 1
+        groups = [[d for d in g if d] for g in groups]
+        return groups.count(self.damaged)
 
 
 def solve(text: str) -> int:
