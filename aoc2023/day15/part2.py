fe9a3650a0663a22a1a944f990707f075d2aa586
Michael T <bravmech@gmail.com>
2023-12-19 16:43:05 +0400

    Solve day 15 part 2

--- aoc2023/day15/part2.py
+++ aoc2023/day15/part2.py
@@ -1,5 +1,54 @@
+import collections
+import dataclasses
+
+from . import part1
+
+
+@dataclasses.dataclass
+class Box:
+    lenses: list[tuple[str, int]] = dataclasses.field(default_factory=list)
+
+    def add(self, label: str, length: int) -> None:
+        lens = label, length
+        for i, (l, _) in enumerate(self.lenses):
+            if l == label:
+                self.lenses[i] = lens
+                return
+        self.lenses.append(lens)
+
+    def remove(self, label: str) -> None:
+        for i, (l, _) in enumerate(self.lenses):
+            if l == label:
+                del self.lenses[i]
+                return
+
+
+def focusing_power(boxes: dict[int, Box]) -> int:
+    result = 0
+    for num, box in boxes.items():
+        for i, (_, length) in enumerate(box.lenses, 1):
+            result += (1 + num) * i * length
+    return result
+
+
+# TODO: refactor parsing
 def solve(text: str) -> int:
-    return 0
+    steps = text.strip().split(',')
+    boxes: dict[int, Box] = collections.defaultdict(Box)
+    for step in steps:
+        parts = step.split('=')
+        if len(parts) == 2:
+            label, length = parts[0], int(parts[1])
+            h = part1.run_hash(label)
+            boxes[h].add(label, length)
+            continue
+        parts = step.split('-')
+        if len(parts) == 2:
+            label, _ = parts
+            h = part1.run_hash(label)
+            boxes[h].remove(label)
+            continue
+    return focusing_power(boxes)
 
 
 if __name__ == '__main__':
--- aoc2023/day15/tests.py
+++ aoc2023/day15/tests.py
@@ -17,12 +17,11 @@ def test_part1(filename: str, expected: int):
     assert part1.solve(text) == expected
 
 
-@pytest.mark.skip
 @pytest.mark.parametrize(
     'filename, expected',
     [
-        ('example.txt', ...),
-        ('input.txt', ...),
+        ('example.txt', 145),
+        ('input.txt', 303404),
     ],
 )
 def test_part2(filename: str, expected: int):
