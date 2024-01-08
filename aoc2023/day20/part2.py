e95652290b0ea8fb2daf5e6b7f84e1d7ad938de0
Michael T <bravmech@gmail.com>
2024-01-08 09:58:56 +0400

    Solve day 20 part 2

--- aoc2023/day20/part2.py
+++ aoc2023/day20/part2.py
@@ -1,5 +1,45 @@
-def solve(text: str) -> int:
+import collections
+import functools
+import math
+
+from . import part1
+
+MAX_TIMES = 10000
+
+
+def push_button(graph: dict[str, list[str]], modules: dict[str, part1.Module]) -> int:
+    counter = dict.fromkeys({'sg', 'lm', 'dh', 'db'}, 0)
+    for times in range(1, MAX_TIMES + 1):
+        queue = collections.deque([('broadcaster', False)])
+        while queue:
+            v, sig1 = queue.popleft()
+            if v not in graph:
+                continue
+            if sig1 and counter.get(v) == 0:
+                counter[v] = times
+                if all(counter.values()):
+                    return functools.reduce(math.lcm, counter.values())
+
+            for w in graph[v]:
+                if w not in modules:
+                    continue
+                sig2 = modules[w].send(v, sig1)
+                if sig2 is not None:
+                    queue.append((w, sig2))
     return 0
+
+
+def solve(text: str) -> int:
+    lines = text.splitlines()
+    graph = part1.parse_graph(lines)
+    modules = {
+        module.name: module for module in (part1.parse_modules(line) for line in lines)
+    }
+    for v in graph:
+        for w in graph[v]:
+            if w in modules and isinstance(modules[w], part1.Conjunction):
+                modules[w].send(v, False)
+    return push_button(graph, modules)
 
 
 if __name__ == '__main__':
--- aoc2023/day20/tests.py
+++ aoc2023/day20/tests.py
@@ -10,7 +10,7 @@ from . import part1, part2
     [
         ('example1.txt', 32000000),
         ('example2.txt', 11687500),
-        ('input.txt', ...),
+        ('input.txt', 841763884),
     ],
 )
 def test_part1(filename: str, expected: int):
@@ -18,12 +18,10 @@ def test_part1(filename: str, expected: int):
     assert part1.solve(text) == expected
 
 
-@pytest.mark.skip
 @pytest.mark.parametrize(
     'filename, expected',
     [
-        ('example.txt', ...),
-        ('input.txt', ...),
+        ('input.txt', 246006621493687),
     ],
 )
 def test_part2(filename: str, expected: int):
